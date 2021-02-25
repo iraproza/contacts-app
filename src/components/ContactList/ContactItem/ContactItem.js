efcb96c85d1d22f31224c4c66bb169b9be05d15c
iraproza <kolomiira@gmail.com>
2021-02-25 20:59:27 +0200

    State + props

--- src/components/ContactList/ContactItem/ContactItem.js
+++ src/components/ContactList/ContactItem/ContactItem.js
@@ -1,9 +1,43 @@
 import React from "react";
 
-const ContactItem = () => {
+const ContactItem = ({Avatar, Name, Created, Role, Status, Email }) => {
     return(
-        <h3>Contact Item</h3>
+        <tr>
+		    <td>
+				<img src={Avatar} alt=""/>
+				<a href="#" className="user-link">{Name}</a>
+				<span className="user-subhead">{Role}</span>
+			</td>
+            <td>
+               {Created}
+            </td>
+            <td className="text-center">
+                <span className="label label-default">{Status}</span>
+            </td>
+            <td>
+                <a href="#">{Email}</a>
+            </td>
+            <td>
+                <a href="#" className="table-link">
+                    <span className="fa-stack">
+                        <i className="fa fa-square fa-stack-2x"></i>
+                        <i className="fa fa-search-plus fa-stack-1x fa-inverse"></i>
+                    </span>
+                </a>
+                <a href="#" className="table-link">
+                    <span className="fa-stack">
+                        <i className="fa fa-square fa-stack-2x"></i>
+                        <i className="fa fa-pencil fa-stack-1x fa-inverse"></i>
+                    </span>
+                </a>
+                <a href="#" className="table-link danger">
+                    <span className="fa-stack">
+                        <i className="fa fa-square fa-stack-2x"></i>
+                        <i className="fa fa-trash-o fa-stack-1x fa-inverse"></i>
+                    </span>
+                </a>
+            </td>
+		</tr>
     )
 }
-
 export default ContactItem;
--- src/index.js
+++ src/index.js
@@ -1,22 +1,46 @@
-import React, { Fragment } from "react";
+import React, { Fragment, Component } from "react";
 import ReactDOM from "react-dom";
 import "./index.css";
 
 // Components
-import Header from "./components/Header/Header";
-import Search from "./components/Search/Search";
-import ContactList from "./components/ContactList/ContactList";
-import Footer from "./components/Footer/Footer";
+import Header from "./components/Header/header";
+import Search from "./components/Search/search";
+import ContactList from "./components/ContactList/contactList";
+import Footer from "./components/Footer/footer";
 
-const App = () => {
-  return(
-    <Fragment>
-      <Header />
-      <Search/>
-      <ContactList />
-      <Footer/>
-    </Fragment>
-  )
+class App extends Component {
+  state = {
+    List: [
+      {
+        "Avatar": "https://bootdey.com/img/Content/avatar/avatar3.png",
+        "Name": "Mila Kunis",
+        "Created": "2013/08/08",
+        "Role": "Admin",
+        "Status": "Active",
+        "Email": "mila@kunis.com"
+      },
+      {
+        "Avatar": "https://bootdey.com/img/Content/avatar/avatar2.png",
+        "Name": "Camil Jonson",
+        "Created": "2013/08/08",
+        "Role": "User",
+        "Status": "Inactive",
+        "Email": "camil@gmail.com"
+      }
+    ]
+  }
+
+  render(){
+    const { List } = this.state;
+    return(
+      <Fragment>
+        <Header />
+        <Search />
+        <ContactList List={List} />
+        <Footer />
+      </Fragment>
+    )
+  }
 }
 
 ReactDOM.render(<App />, document.getElementById("root"));
